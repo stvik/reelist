bb1543acb6009921868f4553ed4951404b3a0357
stvik <stvik2@gmail.com>
2019-12-19 10:19:28 -0500

    actual first commit

---
+++ movie-backend/db/migrate/20191219151410_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.float :rating
+      t.text :description
+      t.string :picture
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
