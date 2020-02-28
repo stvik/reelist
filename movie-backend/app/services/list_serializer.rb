b51f68a5d77dcbab860d9c77c3ddcadffa53063e
stvik <stvik2@gmail.com>
2020-02-28 15:00:19 -0500

    restructered list objects

--- movie-backend/app/services/list_serializer.rb
+++ movie-backend/app/services/list_serializer.rb
@@ -7,8 +7,12 @@ class ListSerializer
 
 	def to_serialized_json
 		@list.to_json(:include => {
-		:movies => {except: [:created_at, :updated_at]},
-		:adds => {except: [:created_at, :updated_at]}
+		:movies => {
+			:include =>{
+				:adds => {except: [:created_at, :updated_at]}
+			},
+			:except => [:created_at, :updated_at]}
+		
 		},
 		:except => [:updated_at])
 	end
