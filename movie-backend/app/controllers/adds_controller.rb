e4242ecacdb8a959a6eeb746fd164ce327879b6b
stvik <stvik2@gmail.com>
2020-03-02 17:30:55 -0500

    changed routes

--- movie-backend/app/controllers/adds_controller.rb
+++ movie-backend/app/controllers/adds_controller.rb
@@ -1,15 +1,5 @@
 class AddsController < ApplicationController
 
-	# def index 
-	# 	adds = Add.all
-	# 	render json: adds
-	# end
-
-	# def show
-	# 	add = Add.find(params[:id])
-	# 	rend json: add
-	# end
-
 	def create
 		add = Add.create(adds_params)
 		render json: add
--- movie-backend/config/routes.rb
+++ movie-backend/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :adds
-  resources :lists
-  post '/movies/search', to: "movies#search"
-  get '/movies/random', to: "movies#random"
-  resources :movies
+  resources :adds, only: [:create, :destroy]
+  resources :lists, only: [:index, :show, :create, :destroy]
+  resources :movies, only: [:index, :create, :show]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
